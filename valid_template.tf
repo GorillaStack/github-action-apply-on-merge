23d0c8488dbc23ea0701d9964dc6dad5b4a5ad3f
Elliott Spira <elliott@gorillastack.com>
2019-08-06 17:52:55 +1000

    try deleting a tag group

--- valid_template.tf
+++ valid_template.tf
@@ -11,7 +11,7 @@ resource "gorillastack_tag_group" "ec2_instance_targets" {
   tag_expression = "i \"application\":\"crm\" and not /environment/:/production/"
 }
 
-resource "gorillastack_tag_group" "autoscaling_group_targets" {
-  name    = "AutoScaling Group Targets"
-  tag_expression = "i \"type\":\"api\" and not /environment/:/production/"
-}
+# resource "gorillastack_tag_group" "autoscaling_group_targets" {
+#   name    = "AutoScaling Group Targets"
+#   tag_expression = "i \"type\":\"api\" and not /environment/:/production/"
+# }
